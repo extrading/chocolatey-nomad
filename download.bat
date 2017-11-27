0142d6be97c2e5e515e162ea973aabf361e57535
Lukas Praninskas <l.praninskas@gmail.com>
2017-11-27 15:04:17 +0200

    Change more missed consul statements

--- download.bat
+++ download.bat
@@ -2,7 +2,7 @@ if "%NOMAD_VERSION%"=="" exit /b 1
 if exist binaries rmdir /s /q binaries
 mkdir binaries
 pushd binaries
-curl -k -fsSL https://releases.hashicorp.com/nomad//%NOMAD_VERSION%/consul_%NOMAD_VERSION%_windows_amd64.zip > %NOMAD_VERSION%_windows_amd64.zip
-curl -k -fsSL https://releases.hashicorp.com/nomad//%NOMAD_VERSION%/consul_%NOMAD_VERSION%_windows_386.zip > %NOMAD_VERSION%_windows_386.zip
-curl -k -fsSL https://releases.hashicorp.com/nomad//%NOMAD_VERSION%/consul_%NOMAD_VERSION%_SHA256SUMS > %NOMAD_VERSION%_SHA256SUMS
+curl -k -fsSL https://releases.hashicorp.com/nomad//%NOMAD_VERSION%/nomad_%NOMAD_VERSION%_windows_amd64.zip > %NOMAD_VERSION%_windows_amd64.zip
+curl -k -fsSL https://releases.hashicorp.com/nomad//%NOMAD_VERSION%/nomad_%NOMAD_VERSION%_windows_386.zip > %NOMAD_VERSION%_windows_386.zip
+curl -k -fsSL https://releases.hashicorp.com/nomad//%NOMAD_VERSION%/nomad_%NOMAD_VERSION%_SHA256SUMS > %NOMAD_VERSION%_SHA256SUMS
 popd
